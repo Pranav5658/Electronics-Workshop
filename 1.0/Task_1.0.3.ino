93ec3622a8f2274d773dafce3f43252d19711c75
Pranav <pranav.ravis@gmail.com>
2026-02-07 21:57:10 +0530

    Update Task_1.0.3.ino

--- 1.0/Task_1.0.3.ino
+++ 1.0/Task_1.0.3.ino
@@ -1,1 +1,27 @@
+bool led = LOW;
+bool pButton = HIGH;
+
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(11, OUTPUT);
+
+  digitalWrite(11, LOW);
+}
+
+void loop() {
+  bool cButton = digitalRead(2);
+
+  if (pButton == LOW && cButton == HIGH) {
+    if(led == LOW){
+      led = HIGH;
+    }else{
+      led = LOW;
+    }
+     
+    digitalWrite(11, led);
+    delay(50);                       
+  }
+
+  pButton = cButton;
+}
 
