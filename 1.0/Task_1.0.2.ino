d30b6328e6cc8b68fc7d2e5932a113e69a659854
Pranav <pranav.ravis@gmail.com>
2026-02-07 21:56:47 +0530

    Update Task_1.0.2.ino

--- 1.0/Task_1.0.2.ino
+++ 1.0/Task_1.0.2.ino
@@ -1,1 +1,15 @@
+void setup() {
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  for(int i=0; i<256; i++){
+  	analogWrite(11,i);
+    delay(2);
+  }
+  for(int i=255; i>=0; i--){
+  	analogWrite(11,i);
+    delay(2);
+  }                      
+}
 
