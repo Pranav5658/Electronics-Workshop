868ff71033cbf915fb08e8826dc1123a0dc1218a
Pranav <pranav.ravis@gmail.com>
2026-02-07 21:54:57 +0530

    Update Task1.1.5.ino

--- 1.1/Task1.1.5.ino
+++ 1.1/Task1.1.5.ino
@@ -1,1 +1,15 @@
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+}
 
+void loop() {
+  int Sound = digitalRead(2);
+
+  if (Sound == HIGH) {
+    digitalWrite(8, HIGH);
+    delay(2000);            
+    digitalWrite(8, LOW);
+  }
+}
