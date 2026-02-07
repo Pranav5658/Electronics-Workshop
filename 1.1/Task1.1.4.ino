7e717b2a1cd034f41f0bc14345fdcd7eac30c113
Pranav <pranav.ravis@gmail.com>
2026-02-07 21:54:41 +0530

    Add ultrasonic distance measurement functionality
    Implement ultrasonic distance measurement using pin 8 for output and pin 9 for input.

--- 1.1/Task1.1.4.ino
+++ 1.1/Task1.1.4.ino
@@ -1,1 +1,34 @@
+float Distance, Duration;
+
+void setup()
+{
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(8, LOW);
+  delayMicroseconds(2);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(8, LOW);
+
+  Duration = pulseIn(9, HIGH, 30000);
+  Distance = (Duration*0.343)/20;
+
+  if(Duration == 0)
+  {
+    Serial.println("No Object Detected");
+  }
+  else
+  {
+    Serial.println(Duration);
+    Serial.println(Distance);
+
+  }
+  
+  delay(500);
+}
 
